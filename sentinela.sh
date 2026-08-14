c69ebde211b5fa9f8dd15eccc42608848fb1fc2d
Ricardo Nogueira de Souza <ricardo18informatica2010@gmail.com>
2026-08-14 07:50:18 +0000

    Lab1: sentinela de disponibilidade com cron

---
+++ sentinela.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# sentinela.sh - verifica se um site esta no ar
+
+SITE="https://www.sankhya.com.br"
+LOG="/home/ricardo/lab1/sentinela.log"
+
+AGORA=$(date '+%d/%m/%Y %H:%M:%S')
+CODIGO=$(curl -s -o /dev/null -w "%{http_code}" --max-time 10 "$SITE")
+
+if [ "$CODIGO" = "200" ]; then
+    echo "$AGORA | $SITE | $CODIGO | OK" >> "$LOG"
+else
+    echo "$AGORA | $SITE | $CODIGO | FALHA" >> "$LOG"
+fi
