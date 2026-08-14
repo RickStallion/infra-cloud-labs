831c235af7ffb5e181e798b9d2fad889f03036db
Ricardo Nogueira de Souza <ricardo18informatica2010@gmail.com>
2026-08-14 12:56:38 +0000

    Corrige caminho do log apos mover para lab1/

--- lab1/sentinela.sh
+++ lab1/sentinela.sh
@@ -2,7 +2,7 @@
 # sentinela.sh - verifica se um site esta no ar
 
 SITE="https://www.sankhya.com.br"
-LOG="/home/ricardo/lab1/sentinela.log"
+LOG="/home/ricardo/lab1/lab1/sentinela.log"
 
 AGORA=$(date '+%d/%m/%Y %H:%M:%S')
 CODIGO=$(curl -s -o /dev/null -w "%{http_code}" --max-time 10 "$SITE")
